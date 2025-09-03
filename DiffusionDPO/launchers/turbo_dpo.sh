ebbb1f594ddde6196edcc1be315e0083f4bdf024
ken <1610057945@qq.com>
2025-09-03 16:12:04 +0800

    Continue running

--- DiffusionDPO/launchers/turbo_dpo.sh
+++ DiffusionDPO/launchers/turbo_dpo.sh
@@ -13,11 +13,10 @@ accelerate launch train.py \
   --max_train_steps=3000 \
   --dataloader_num_workers=0 \
   --gradient_accumulation_steps=16 \
-  --num_train_epochs=3 \
   --lr_scheduler="constant_with_warmup" --lr_warmup_steps=5 \
   --learning_rate=1e-8 --scale_lr \
   --cache_dir="~/.cache" \
   --checkpointing_steps 10 \
   --beta_dpo 5000 \
   --sdxl --resolution 512 --proportion_empty_prompts 0 \
-  --output_dir="try-improve-1"
+  --output_dir="try-improve-2-more-steps"
